23b4707f118c19ff6d8a56294d8058574d9ae1f0
katewayc <kate.bqool@gmail.com>
2019-05-10 18:30:49 +0800

    修改檔案名稱及路徑

---
+++ source/ASP_NET_MVC_Q1/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP_NET_MVC_Q1.Models;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace ASP_NET_MVC_Q1.Controllers
+{
+    public class DataController : Controller
+    {
+        public ContentResult Save(Customer customer)
+        {
+            var json = new JavaScriptSerializer().Serialize(customer);
+
+            StreamWriter sw = new StreamWriter(Server.MapPath(@"~/App_Data/save_customer.txt"),true);
+            sw.WriteLine(json);
+            sw.Close();
+
+            return Content("存檔路徑 : "+Server.MapPath(@"~/App_Data/save_customer.txt")+ "<br><br>存檔內容 : " + json);
+        }
+    }
+}
---
+++ source/ASP_NET_MVC_Q1/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_NET_MVC_Q1.Models
+{
+    public class Customer
+    {
+        [Required]
+        [StringLength(20,MinimumLength =3,ErrorMessage ="at least 3 letters")]
+        [Remote(action: "Verify", controller: "Customer")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{4}\-?\d{3}\-?\d{3}$",ErrorMessage ="phone format has to be 09xx-xxx-xxx")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage ="please enter Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
